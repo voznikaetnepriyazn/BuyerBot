fa59a09a11e639adaa6db0ee576f5df4234e8e75
Daria Maslova <maslovadaria16.07@gmail.com>
2025-11-10 00:03:55 +0300

    configuration of logger

--- main.go
+++ main.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"fmt"
-	"url-shortener/internal/config"
+	"os"
+	"log/slog"
+	"url-shortener\internall\config"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg) //это надо хранить в секретах
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+
+	log.Debug("debug messages are enabled")
+}
+
+func setUpLogger(env string) {//конфигурация логгера
+	var log *slog.Logger
+
+	switch env{
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }

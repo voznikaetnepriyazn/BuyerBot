745c67bdc9c75f2d33498d27413a2c7f8c028d6b
Daria Maslova <maslovadaria16.07@gmail.com>
2025-11-12 21:14:59 +0300

    switch to json

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,21 +11,21 @@ import (
 //парсер конфига
 
 type Config struct {
-	Env        string `yaml:"env" env-default:"local" env-required:"true"`
-	HttpServer `yaml:"http_server"`
-	//db
+	Env         string `json:"env" env-default:"local" env-required:"true"`
+	HttpServer  `json:"http_server"`
+	StoragePath string `json:"storage_path" env-required:"true"`
 }
 
 type HttpServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8080"`
-	Timeout     time.Duration `yaml:"timeout" env-default:"10s"`
-	IdleTimeout time.Duration `yaml:"iddle-timeout" env-default:"60s"`
+	Address     string        `json:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `json:"timeout" env-default:"10s"`
+	IdleTimeout time.Duration `json:"idle-timeout" env-default:"60s"`
 }
 
 // инициализация - конструктор
 func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH") //переменная окружения
-	if configPath == " " {
+	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
 	}
 

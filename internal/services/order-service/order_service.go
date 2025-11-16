cc8129e8d35fba0e0a25ca99fe9e23b34c809d41
Daria Maslova <maslovadaria16.07@gmail.com>
2025-11-16 12:41:15 +0300

    business logic

--- internal/services/order-service/order_service.go
+++ internal/services/order-service/order_service.go
@@ -10,6 +10,26 @@
 
 package services
 
+import (
+	"Order/internal/storage"
+)
+
+type OrderStruct struct {
+	storage storage.StorageInter
+}
 type OrderService interface {
 	IsOrderCreated(id int) bool
+}
+
+func (o *OrderStruct) IsOrderCreated(id int64) (bool, error) {
+	order, err := o.storage.GetByIdURL(id)
+	if err != nil {
+		return false, err
+	}
+
+	if order == 0 {
+		return false, err
+	}
+
+	return true, nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,3 +6,15 @@ var (
 	ErrUrlNotFound = errors.New("url not found")
 	ErrUrlExist    = errors.New("url exist")
 )
+
+type StorageInter interface {
+	AddURL(urlToSave string, alias string) (int64, error)
+
+	DeleteURL(urlToSave int64) error
+
+	GetAllURL() ([]int64, error)
+
+	GetByIdURL(id int64) (int64, error)
+
+	UpdateURL(oldUrl string, urlToSave string, alias string) error
+}

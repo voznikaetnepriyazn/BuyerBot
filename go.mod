e95886ecd25cae932f5bb7c035ae8b4ded53a2fb
Daria Maslova <maslovadaria16.07@gmail.com>
2026-01-08 20:31:38 +0300

    config

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module telegram
 
 go 1.25.4
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+require github.com/caarlos0/env/v6 v6.10.1 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,19 @@
 package config
+
+import "github.com/caarlos0/env/v6"
+
+type Config struct {
+	TelegramBotToken string `env:"TELEGRAM_BOT_TOKEN" envDefault:""`
+
+	OrderServiceURL    string `env:"ORDER_SERVICE_URL" envDefault:"http://localhost:8081"`
+	CustomerServiceURL string `env:"CUSTOMER_SERVICE_URL" envDefault:"http://localhost:8082"`
+	GoodServiceURL     string `env:"GOOD_SERVICE_URL" envDefault:"http://localhost:8083"`
+}
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
